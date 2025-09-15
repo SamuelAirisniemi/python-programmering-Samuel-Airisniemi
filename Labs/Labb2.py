614eb9049503de6ac10338beba1953c1b9888e07
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-09-15 15:49:26 +0200

    gjort diagram

--- Labs/Labb2.py
+++ Labs/Labb2.py
@@ -4,4 +4,18 @@ import pandas as pd
 
 df = pd.read_csv("Labs\datapoints.txt")
 
-print(df.head())
+x = df["(width (cm)"]
+y = df[" height (cm)"]
+z = df[" label (0-pichu"]
+
+plt.scatter(x[z == 0], y[z == 0], color='black', label='Pichu', alpha = 0.5)
+plt.scatter(x[z == 1], y[z == 1], color='yellow', label='Pikachu', alpha = 0.5)
+
+plt.title("Pichu vs Pikachu")
+plt.xlabel("Widht (cm)")
+plt.ylabel("Height (cm)")
+plt.legend()
+plt.grid()
+plt.show()
+
+#print(df.head())
