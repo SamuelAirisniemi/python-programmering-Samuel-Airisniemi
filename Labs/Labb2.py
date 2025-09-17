f7e3eedbbd819b41f3aea1a9d150929a1c9c054a
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-09-17 14:33:06 +0200

    fix

--- Labs/Labb2.py
+++ Labs/Labb2.py
@@ -1,21 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import re
 
 df = pd.read_csv("Labs/datapoints.txt")
-test_df = pd.read_csv("Labs/testpoints.csv")
 df.columns = df.columns.str.strip()
 
 x = df["(width (cm)"]
 y = df["height (cm)"]
 z = df["label (0-pichu"]
-test_x = ["widht"]
-test_y = ["height"]
 
+test_x = []
+test_y = []
 
-plt.scatter(x[z == 0], y[z == 0], color = "black", label = "Pichu", alpha = 0.5)
-plt.scatter(x[z == 1], y[z == 1], color = "yellow", label = "Pikachu", alpha=0.5)
-plt.scatter(test_x, test_y, color = "red", label = "Test Points")
+with open("Labs/testpoints.txt", "r") as f:
+    for line in f:
+        match = re.search(r"\(([\d\.]+),\s*([\d\.]+)\)", line)
+        if match:
+            test_x.append(float(match.group(1)))
+            test_y.append(float(match.group(2)))
+
+plt.scatter(x[z == 0], y[z == 0], color='black', label='Pichu', alpha=0.5)
+plt.scatter(x[z == 1], y[z == 1], color='yellow', label='Pikachu', alpha=0.5)
+plt.scatter(test_x, test_y, color='red', label='Test Points')
 
 plt.title("Pichu vs Pikachu")
 plt.xlabel("Width (cm)")
