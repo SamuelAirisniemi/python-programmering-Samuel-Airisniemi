fde9bf7595622023efcd98f8a276491a11ef7c61
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-09-17 15:59:04 +0200

    gjort övning

--- Exercises/list exercises/MonteCarloSimulation.py
+++ Exercises/list exercises/MonteCarloSimulation.py
@@ -1,11 +1,23 @@
 import math
 import matplotlib.pyplot as plt
+import random
 import numpy as np
 
-x = np.array([-1, 1])
-y = np.array([-1, 1])
+points = 5000
+x = np.random.uniform(-1, 1, points)
+y = np.random.uniform(-1, 1, points)
+d = np.sqrt(x**2 + y**2)
 
-d = ((x**2 + y**2))**0.5
+inside = d < 1
+outside = d >= 1
 
-plt.plot(x, y)
+plt.figure(figsize=(8, 8))
+plt.scatter(x[inside], y[inside], color = "blue", label = "(d < 1)")
+plt.scatter(x[outside], y[outside], color = "orange", label = "(d > 1)")
+plt.gca().set_aspect("equal", adjustable="box")
+plt.title("Simulation of 5000 points")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid()
 plt.show()
