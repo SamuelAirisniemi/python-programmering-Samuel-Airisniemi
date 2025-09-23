1bf665eb2d2ec729acf2be0fa452eec2c2304abc
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-09-23 22:26:52 +0200

    idk

--- Labs/Labb2.py
+++ Labs/Labb2.py
@@ -4,8 +4,6 @@ import numpy as np
 width = []
 height = []
 label = []
-testx = []
-testy = []
 
 with open("Labs/datapoints.txt", "r") as file:
     next(file)
@@ -16,16 +14,28 @@ with open("Labs/datapoints.txt", "r") as file:
             height.append(float(parts[1]))
             label.append(int(parts[2]))
 
-with open("Labs/testpoints.txt", "r") as file:
-    for line in file:
-        if "(" in line:
-            coords = line[line.find("(")+1 : line.find(")")].split(",")
-            testx.append(float(coords[0]))
-            testy.append(float(coords[1]))
-
 data_points = np.array(list(zip(width, height)))
-train_labels = np.array(label)
-test_points = np.array(list(zip(testx, testy)))
+full_labels = np.array(label)
+
+indices = np.arange(len(data_points))
+np.random.shuffle(indices)
+data_points = data_points[indices]
+full_labels = full_labels[indices]
+
+pichu_count = np.sum(full_labels == 0)
+pikachu_count = np.sum(full_labels == 1)
+
+if pichu_count >= 75 and pikachu_count >= 75:
+    train_points = np.concatenate([data_points[full_labels == 0][:50], data_points[full_labels == 1][:50]])
+    train_labels = np.concatenate([full_labels[full_labels == 0][:50], full_labels[full_labels == 1][:50]])
+    test_points = np.concatenate([data_points[full_labels == 0][50:75], data_points[full_labels == 1][50:75]])
+    test_labels = np.concatenate([full_labels[full_labels == 0][50:75], full_labels[full_labels == 1][50:75]])
+else:
+    train_points = data_points
+    train_labels = full_labels
+    test_points = np.array([])
+    test_labels = np.array([])
+
 label_names = {0: "Pichu", 1: "Pikachu"}
 
 k = 10
@@ -41,13 +51,54 @@ def knn_classify(point, data_points, train_labels, k = 10):
     else:
         return 1
 
-predictions = [knn_classify(point, data_points, train_labels, k) for point in test_points]
+predictions = [knn_classify(point, train_points, train_labels, k) for point in test_points]
 
-for (x_cord, y_cord), pred in zip(test_points, predictions):
+if len(test_labels) > 0:
+    TP = sum((p == 1 and a == 1) for p, a in zip(predictions, test_labels))
+    TN = sum((p == 0 and a == 0) for p, a in zip(predictions, test_labels))
+
+    accuracy = (TP + TN) / len(test_labels)
+
+    print(f"TP = {TP}, TN = {TN}")
+    print(f"Accuracy = {accuracy:.2f}")
+
+testx = []
+testy = []
+
+with open("Labs/testpoints.txt", "r") as file:
+    for line in file:
+        if "(" in line:
+            coords = line[line.find("(")+1 : line.find(")")].split(",")
+            testx.append(float(coords[0]))
+            testy.append(float(coords[1]))
+
+test_points_cord = np.array(list(zip(testx, testy)))
+
+predictions_external = [knn_classify(point, train_points, train_labels, k) for point in test_points_cord]
+for (x_cord, y_cord), pred in zip(test_points_cord, predictions_external):
     print(f"Test point ({x_cord}, {y_cord}) identified as: {label_names[pred]}")
 
-print("Datapunkter", np.array(list(zip(width, height, label))))
+#print("Datapunkter", np.array(list(zip(width, height, label))))
 print("Testpunkter:", list(zip(testx, testy)))
+
+#Fixa h
+plt.scatter([w for (w, h), l in zip(train_points, train_labels) if l == 0], [h for (w, h), l in zip(train_points, train_labels) if l == 0], color = "black", label = "Pichu", alpha = 0.5)
+plt.scatter([w for (w, h), l in zip(train_points, train_labels) if l == 1], [h for (w, h), l in zip(train_points, train_labels) if l == 1], color = "yellow", label = "Pikachu", alpha = 0.5)
+
+if len(test_points) > 0:
+    for (x, y), pred, actual in zip(test_points, predictions, test_labels):
+        if pred == actual:
+            plt.scatter(x, y, color = "green", alpha = 0.5, marker = "x")
+        else:
+            plt.scatter(x, y, color = "red", alpha = 0.5, marker = "x")
+
+plt.scatter(testx, testy, color = "orange", label = "Testpoints")
+
+plt.title("Pichu vs Pikachu")
+plt.xlabel("Width (cm)")
+plt.ylabel("Height (cm)")
+plt.legend()
+plt.grid()
 
 def identify_user_input():
     while True:
@@ -71,7 +122,7 @@ def identify_user_input():
             continue
 
         user_point = np.array([width_input, height_input])
-        predicted_label = knn_classify(user_point, data_points, train_labels, k)
+        predicted_label = knn_classify(user_point, train_points, train_labels, k)
         identification = label_names[predicted_label]
 
         print(f"Point ({width_input}, {height_input}) is identified as {identification}")
@@ -80,13 +131,5 @@ def identify_user_input():
         plt.legend() 
         break
 
-plt.scatter([w for w, l in zip(width, label) if l == 0], [h for h, l in zip(height, label) if l == 0], color = "black", label = "Pichu", alpha = 0.5)
-plt.scatter([w for w, l in zip(width, label) if l == 1], [h for h, l in zip(height, label) if l == 1], color = "yellow", label = "Pikachu", alpha = 0.5)
-plt.scatter(testx, testy, color = "red", marker = "*", label = "Testpoints")
-plt.title("Pichu vs Pikachu")
-plt.xlabel("Width (cm)")
-plt.ylabel("Height (cm)")
-plt.legend()
-plt.grid()
 identify_user_input()
 plt.show()
