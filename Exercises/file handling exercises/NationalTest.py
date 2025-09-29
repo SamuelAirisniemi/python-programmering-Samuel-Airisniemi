d6666794f9b18b26f0688fa7ddf03d398e5b9e4f
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-09-29 13:18:35 +0200

    övninvgar

---
+++ Exercises/file handling exercises/NationalTest.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+path2A = "Exercises/file handling exercises/NPvt19Ma2A.txt"
+path2C = "Exercises/file handling exercises/NPvt19Ma2C.txt"
+
+with open("Exercises/file handling exercises/NPvt19Ma2A.txt") as f:
+    print(f.read)
