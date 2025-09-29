a02be3247eeb9c302681b0bc377620ada68724b2
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-09-29 11:07:50 +0200

    ny övning

--- Exercises/file handling exercises/dice_rolls.py
+++ Exercises/file handling exercises/dice_rolls.py
@@ -1,6 +1,13 @@
 import random as rnd
 
+path = "Exercises/file handling exercises/dice_rolls.txt"
 dice_roll = [rnd.randint(1, 6) for i in range(20)]
+sorted_rolls = sorted(dice_roll)
+fours = [i+1 for i, roll in enumerate(dice_roll) if roll == 4]
 
-print("Simulate 20 dice rolls:")
-print(dice_roll)
+with open(path, "w") as file:
+    file.write("Simulate 20 dice rolls:\n")
+    file.write(f"{dice_roll}\n\n")
+    file.write("Sorted dice rolls\n")
+    file.write(f"{sorted_rolls}\n\n")
+    file.write(f"Number of fours: {len(fours)}")
