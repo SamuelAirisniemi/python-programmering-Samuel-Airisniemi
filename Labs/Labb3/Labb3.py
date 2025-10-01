120796c7d22f1ebb6c7f357911a38d63d490c23a
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-10-01 12:06:12 +0200

    förbättrat, delat in i färger, skapat labelled fil med 0 och 1 kolumn

--- Labs/Labb3/Labb3.py
+++ Labs/Labb3/Labb3.py
@@ -1,32 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def point_position(x_point, y_point, k, m):
-    y_line = k * x_point + m
-    if y_point > y_line:
-        vertical_position = "Above"
-    elif y_point > y_line:
-        vertical_position = "Below"
-    else:
-        vertical_position = "On"
-    
-    if k != 0:
-        x_line = (y_point - m) / k
-        if x_point > x_line:
-            horizontal_position = "Right of"
-        elif x_point < x_line:
-            horizontal_position = "Left of"
-        else:
-            horizontal_position = "On"
-    else:
-        horizontal_position = "On"
-    return (f"The point ({x_point}, {y_point}) is {vertical_position} and {horizontal_position} the line")
+def point_side(x_point, y_point, k, m):
+    x1, y1 = 0, m
+    x2, y2 = 1, k*1 + m
 
-path = "Labs/Labb3/unlabelled_data (1).csv"
+    cross = (x2 - x1) * (y_point - y1) - (y2 - y1) * (x_point - x1)
+
+    if cross > 0:
+        return 1
+    else:
+        return 0
+    
+unlabelled = "Labs/Labb3/unlabelled_data (1).csv"
+labelled = "Labs/Labb3/labelled_data.csv"
+
 x, y = [], []
 
-with open(path, "r") as f:
-    for line in f:
+with open(unlabelled, "r") as f_read:
+    for line in f_read:
         parts = line.strip().split(",")
         if len(parts) == 2:
             x_val, y_val = map(float, parts)
@@ -36,17 +28,26 @@ with open(path, "r") as f:
 k = -2
 m = 0
 
+with open(labelled, "w") as f_write:
+    for x_point, y_point in zip(x, y):
+        side = point_side(x_point, y_point, k, m)
+        f_write.write(f"({x_point:.1f}, {y_point:.1f}), {side}\n")
+
 x_line = np.linspace(min(x), max(x))
 y_line = k * x_line + m
 
+x_left = [xi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 0]
+y_left = [yi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 0]
+x_right = [xi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 1]
+y_right = [yi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 1]
+
+plt.figure(figsize=(8, 6))
 plt.plot(x_line, y_line, label = (f"y = {k}x + {m}"), color = "blue")
-plt.scatter(x, y, label = "Data points", color = "Orange")
+plt.scatter(x_left, y_left, color = "red", label = "Vänster (0)")
+plt.scatter(x_right, y_right, color = "green", label = "Höger (1)")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Labb3")
+plt.title("Labb 3")
 plt.grid()
 plt.legend()
 plt.show()
-
-for x_point, y_point in zip(x, y):
-    print(point_position(x_point, y_point, k, m))
