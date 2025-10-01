1f56fe3de249e2e7dd714663bdcf331d5a2b5818
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2025-10-01 14:32:06 +0200

    linjer för vg

--- Labs/Labb3/Labb3.py
+++ Labs/Labb3/Labb3.py
@@ -42,18 +42,26 @@ with open(labelled, "w") as f_write:
 x_line = np.linspace(min(x), max(x))
 y_line = k * x_line + m
 
+f_line = -0.489 * x_line #VG
+g_line = -2 * x_line + 0.16 #VG
+h_line = 800 * x_line - 120 #VG
+
 x_left = [xi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 0]
 y_left = [yi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 0]
 x_right = [xi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 1]
 y_right = [yi for xi, yi in zip(x, y) if point_side(xi, yi, k, m) == 1]
 
-plt.figure(figsize=(8, 6))
 plt.plot(x_line, y_line, label = (f"y = {k}x + {m}"), color = "blue")
+plt.plot(x_line, f_line, label = "f(x) = -0.489x", color = "purple") #VG
+plt.plot(x_line, g_line, label = "g(x) = -2x + 0.16", color = "yellow") #VG
+plt.plot(x_line, h_line, label = "h(x) = 800x - 120", color = "orange") #VG
 plt.scatter(x_left, y_left, color = "red", label = (f"Vänster (0) - {count_0})"))
 plt.scatter(x_right, y_right, color = "green", label = (f"Höger (1) - {count_1}"))
+plt.figure(figsize=(8, 6))
+plt.title("Labb 3")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Labb 3")
+plt.ylim(-5,5)
 plt.grid()
 plt.legend()
 plt.show()
